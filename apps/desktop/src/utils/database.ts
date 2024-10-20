a967ed75c56a7702114aa1fdfd8e26259478e1d8
bygimenez <davidalbal123@gmail.com>
2024-10-21 00:10:44 +0200

    fix(desktop): :ambulance: fix black screen without supabase api

--- apps/desktop/src/utils/database.ts
+++ apps/desktop/src/utils/database.ts
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const apiKey = import.meta.env.VITE_API_KEY;
 const apiUrl = import.meta.env.VITE_API_URL;
 
-export const supabase = createClient(apiKey, apiUrl)
+export const supabase = (apiKey && apiUrl) ? createClient(apiUrl, apiKey) : null;
