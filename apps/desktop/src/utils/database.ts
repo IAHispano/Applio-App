db22d2bf1c2040c9099345033a2f610c3aa50677
bygimenez <davidalbal123@gmail.com>
2024-10-21 11:58:40 +0200

    fix(desktop): :ambulance: fix black screen if dont have apikey

--- apps/desktop/src/utils/database.ts
+++ apps/desktop/src/utils/database.ts
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const apiKey = import.meta.env.VITE_API_KEY;
 const apiUrl = import.meta.env.VITE_API_URL;
 
-export const supabase = (apiKey && apiUrl) ? createClient(apiUrl, apiKey) : null;
+let supabase;
+
+if (!apiKey || !apiUrl) {
+    console.error('Supabase API key or URL is missing');
+} else {
+    supabase = createClient(apiKey, apiUrl);
+}
+
+export { supabase };
