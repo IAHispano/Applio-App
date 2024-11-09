eb23d1c26c35f4ccdfd952bb517d24002919aac7
bygimenez <davidalbal123@gmail.com>
2024-11-10 00:44:26 +0100

    feat(desktop): :sparkles: add navegation buttons

--- apps/desktop/src/components/layout/titlebar.tsx
+++ apps/desktop/src/components/layout/titlebar.tsx
@@ -28,7 +28,49 @@ export const TitleBar = () => {
 			style={{ zIndex: 200 }}
 			data-tauri-drag-region
 		>
-			<div className="justify-end flex gap-4 px-2" data-tauri-drag-region>
+			<div className="flex justify-between items-center px-2">
+			<div className="justify-start gap-2 flex px-2">
+				<button type="button" onClick={() => window.location.reload()} className="slow hover:text-white">
+				<svg width={14} height={14} className="opacity-70 hover:opacity-100 slow" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M1,12A11,11,0,0,1,17.882,2.7l1.411-1.41A1,1,0,0,1,21,2V6a1,1,0,0,1-1,1H16a1,1,0,0,1-.707-1.707l1.128-1.128A8.994,8.994,0,0,0,3,12a1,1,0,0,1-2,0Zm21-1a1,1,0,0,0-1,1,9.01,9.01,0,0,1-9,9,8.9,8.9,0,0,1-4.42-1.166l1.127-1.127A1,1,0,0,0,8,17H4a1,1,0,0,0-1,1v4a1,1,0,0,0,.617.924A.987.987,0,0,0,4,23a1,1,0,0,0,.707-.293L6.118,21.3A10.891,10.891,0,0,0,12,23,11.013,11.013,0,0,0,23,12,1,1,0,0,0,22,11Z"></path></g></svg>
+				</button>
+				<button type="button" onClick={() => history.back()} className="slow hover:text-white">
+				<svg
+				className="opacity-70 hover:opacity-100 slow"
+				width={18}
+				height={18}
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				aria-hidden="true"
+			>
+				<path
+					fillRule="evenodd"
+					clipRule="evenodd"
+					d="M15.7071 4.29289C16.0976 4.68342 16.0976 5.31658 15.7071 5.70711L9.41421 12L15.7071 18.2929C16.0976 18.6834 16.0976 19.3166 15.7071 19.7071C15.3166 20.0976 14.6834 20.0976 14.2929 19.7071L7.29289 12.7071C7.10536 12.5196 7 12.2652 7 12C7 11.7348 7.10536 11.4804 7.29289 11.2929L14.2929 4.29289C14.6834 3.90237 15.3166 3.90237 15.7071 4.29289Z"
+					fill="#ffffff"
+				/>
+				</svg>
+				</button>
+				<button type="button" onClick={() => history.forward()} className="slow hover:text-white">
+				<svg
+					className="opacity-70 hover:opacity-100 slow"
+					width={18}
+					height={18}
+					viewBox="0 0 24 24"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					aria-hidden="true"
+				>
+					<path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="M8.29289 4.29289C8.68342 3.90237 9.31658 3.90237 9.70711 4.29289L16.7071 11.2929C17.0976 11.6834 17.0976 12.3166 16.7071 12.7071L9.70711 19.7071C9.31658 20.0976 8.68342 20.0976 8.29289 19.7071C7.90237 19.3166 7.90237 18.6834 8.29289 18.2929L14.5858 12L8.29289 5.70711C7.90237 5.31658 7.90237 4.68342 8.29289 4.29289Z"
+						fill="#ffffff"
+					/>
+				</svg>
+				</button>
+			</div>
+			<div className="justify-end flex gap-4" data-tauri-drag-region>
 				<button
 					type="button"
 					onClick={minimized}
@@ -116,6 +158,7 @@ export const TitleBar = () => {
 						/>
 					</svg>
 				</button>
+			</div>
 			</div>
 		</div>
 	);
