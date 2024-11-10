4c609a33d9b759d66932a87d8a62c795df2438bd
bygimenez <davidalbal123@gmail.com>
2024-11-10 01:20:27 +0100

    fix(desktop): :bug: fix navigation buttons

--- apps/desktop/src/components/layout/titlebar.tsx
+++ apps/desktop/src/components/layout/titlebar.tsx
@@ -28,8 +28,12 @@ export const TitleBar = () => {
 			style={{ zIndex: 200 }}
 			data-tauri-drag-region
 		>
-			<div className="flex justify-between items-center px-2">
-			<div className="justify-start gap-2 flex px-2">
+			<div className="flex justify-end items-center px-2">
+			{window.location.pathname !== "/first-time" &&
+			window.location.pathname !== "/pretraineds" &&
+			window.location.pathname !== "/os-not-supported" && 
+			window.location.pathname !== "/beta-access" && (
+			<div className="justify-start ml-auto w-full gap-2 flex px-2">
 				<button type="button" onClick={() => window.location.reload()} className="slow hover:text-white">
 				<svg width={14} height={14} className="opacity-70 hover:opacity-100 slow" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M1,12A11,11,0,0,1,17.882,2.7l1.411-1.41A1,1,0,0,1,21,2V6a1,1,0,0,1-1,1H16a1,1,0,0,1-.707-1.707l1.128-1.128A8.994,8.994,0,0,0,3,12a1,1,0,0,1-2,0Zm21-1a1,1,0,0,0-1,1,9.01,9.01,0,0,1-9,9,8.9,8.9,0,0,1-4.42-1.166l1.127-1.127A1,1,0,0,0,8,17H4a1,1,0,0,0-1,1v4a1,1,0,0,0,.617.924A.987.987,0,0,0,4,23a1,1,0,0,0,.707-.293L6.118,21.3A10.891,10.891,0,0,0,12,23,11.013,11.013,0,0,0,23,12,1,1,0,0,0,22,11Z"></path></g></svg>
 				</button>
@@ -70,6 +74,7 @@ export const TitleBar = () => {
 				</svg>
 				</button>
 			</div>
+			)}
 			<div className="justify-end flex gap-4" data-tauri-drag-region>
 				<button
 					type="button"
