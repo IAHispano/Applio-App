0e150bbaaacbaff922dbd1073e31376b9c6a5ee5
bygimenez <davidalbal123@gmail.com>
2024-11-30 17:03:34 +0100

    chore: update dependencies and add dialog plugin support
    - Updated '@tauri-apps/api' to version 2.1.1 and added '@tauri-apps/plugin-dialog' as a dependency in package.json.
    - Incremented version numbers for various Tauri plugins to 2.1.0 and 2.0.1 in pnpm-lock.yaml.
    - Enhanced Cargo.toml and Cargo.lock to include new dependencies and updated versions for Tauri plugins.
    - Added 'dialog:default' capability in default.json to enable dialog functionality in the application.
    - Refactored lib.rs to initialize the dialog plugin within the Tauri application setup.

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -2,13 +2,13 @@
 
 use declarative_discord_rich_presence::activity::Activity;
 use declarative_discord_rich_presence::{activity, DeclarativeDiscordIpcClient};
+use std::io;
+use std::io::{Read, Write};
+use std::net::TcpListener;
+use std::path::PathBuf;
 use std::process::{Child, Command};
 use std::sync::{Arc, Mutex};
-use std::path::PathBuf;
-use std::io;
-use std::net::TcpListener;
 use tauri::State;
-use std::io::{Write, Read};
 
 fn get_server_path() -> io::Result<PathBuf> {
     let base_dir: PathBuf;
@@ -16,8 +16,14 @@ fn get_server_path() -> io::Result<PathBuf> {
     if cfg!(debug_assertions) {
         base_dir = std::env::current_dir()?;
     } else {
-        base_dir = std::env::current_exe()?.parent()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Executable has no parent directory"))?
+        base_dir = std::env::current_exe()?
+            .parent()
+            .ok_or_else(|| {
+                io::Error::new(
+                    io::ErrorKind::NotFound,
+                    "Executable has no parent directory",
+                )
+            })?
             .to_path_buf();
     }
 
@@ -49,7 +55,6 @@ fn get_server_path() -> io::Result<PathBuf> {
     Ok(server_path)
 }
 
-
 fn find_available_port() -> Option<u16> {
     if let Ok(listener) = TcpListener::bind("127.0.0.1:0") {
         let port = listener.local_addr().unwrap().port();
@@ -62,7 +67,7 @@ fn find_available_port() -> Option<u16> {
 fn start_server(port: u16) -> io::Result<Child> {
     let server_path = get_server_path()?;
     println!("Project root: {:?}", server_path);
-    
+
     let mut child = Command::new(&server_path)
         .arg(port.to_string())
         .stdout(std::process::Stdio::inherit())
@@ -74,20 +79,23 @@ fn start_server(port: u16) -> io::Result<Child> {
             eprintln!("Error details: {:?}", e);
             e
         })?;
-    
+
     println!("Initializing server on port {}...", port);
-    
+
     std::thread::sleep(std::time::Duration::from_secs(1));
-    
+
     match child.try_wait() {
         Ok(Some(status)) => {
             eprintln!("Server process exited immediately with status: {}", status);
-            Err(io::Error::new(io::ErrorKind::Other, "Server exited immediately"))
-        },
+            Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Server exited immediately",
+            ))
+        }
         Ok(None) => {
             println!("Server process running successfully");
             Ok(child)
-        },
+        }
         Err(e) => {
             eprintln!("Error checking server process: {}", e);
             Err(e)
@@ -101,6 +109,7 @@ pub fn run() {
     let port = Arc::new(Mutex::new(None));
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .setup({
             let flask_server = Arc::clone(&flask_server);
@@ -108,16 +117,16 @@ pub fn run() {
 
             move |_app| {
                 if let Some(p) = find_available_port() {
-                match start_server(p) {
-                    Ok(server_process) => {
-                        *flask_server.lock().unwrap() = Some(server_process);
-                        *port_clone.lock().unwrap() = Some(p); 
+                    match start_server(p) {
+                        Ok(server_process) => {
+                            *flask_server.lock().unwrap() = Some(server_process);
+                            *port_clone.lock().unwrap() = Some(p);
+                        }
+                        Err(e) => {
+                            eprintln!("Error starting server: {}", e);
+                        }
                     }
-                    Err(e) => {
-                        eprintln!("Error starting server: {}", e);
-                    }
-                }
-                Ok(())
+                    Ok(())
                 } else {
                     println!("Failed to find an available port.");
                     Ok(())
@@ -128,14 +137,19 @@ pub fn run() {
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_oauth::init())
-        .invoke_handler(tauri::generate_handler![set_discord_presence, is_dev, get_port])
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![
+            set_discord_presence,
+            is_dev,
+            get_port
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
 fn is_dev() -> bool {
-    cfg!(debug_assertions) 
+    cfg!(debug_assertions)
 }
 
 #[tauri::command]
@@ -155,10 +169,10 @@ fn set_discord_presence(state: &str, details: &str) {
 
 #[tauri::command]
 async fn get_port(port: State<'_, Arc<Mutex<Option<u16>>>>) -> Result<u16, String> {
-    let port_guard = port.lock().unwrap(); 
+    let port_guard = port.lock().unwrap();
     if let Some(p) = *port_guard {
         Ok(p)
     } else {
-        Err("Port not available".into()) 
+        Err("Port not available".into())
     }
 }
