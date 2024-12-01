fc8b9355f3222ea959d24f2bc2bb92e0d46cfab5
Blaise <blaise@applio.org>
2024-12-01 21:26:57 +0100

    feat(installer): add close confirmation

--- apps/installer/src/components/titlebar.tsx
+++ apps/installer/src/components/titlebar.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import { window } from "@tauri-apps/api";
 
 export default function Titlebar() {
-	const close = async () => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
+	const confirmClose = async () => {
+		setIsModalOpen(false);
 		await window.getCurrentWindow().close();
 	};
 
@@ -14,10 +26,14 @@ export default function Titlebar() {
 				className="flex justify-between items-center m-auto px-2"
 				data-tauri-drag-region
 			>
-				<p className="text-xs text-neutral-400 title font-medium">
+				<p className="text-sm text-neutral-400 title font-semilight">
 					Applio App Installer
 				</p>
-				<button onClick={close} type="button" className="slow hover:text-white">
+				<button
+					onClick={openModal}
+					type="button"
+					className="slow hover:text-white"
+				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
@@ -36,6 +52,36 @@ export default function Titlebar() {
 					</svg>
 				</button>
 			</div>
+
+			{isModalOpen && (
+				<div className="fixed inset-0 backdrop-blur-md bg-opacity-50 flex justify-center items-center z-50 transition-all duration-500 opacity-100">
+					<div className="bg-[#1c1c1c]/70 p-6 border border-white/20 text-neutral-300 text-sm rounded-xl transition-all duration-300">
+						<h3 className="text-xl font-semibold">Exit Installer?</h3>
+						<p className="my-4 text-sm">
+							You are about to close the installer. Any unsaved progress may be
+							lost. Do you wish to proceed?
+						</p>
+						<div className="flex justify-end space-x-3">
+							<button
+								onClick={closeModal}
+								className="px-4 py-1 text-sm flex justify-center items-center bg-[#1c1c1c]/30 border border-white/20 text-neutral-300 rounded-lg hover:bg-[#1c1c1c]/40 transition-all duration-300"
+							>
+								Cancel
+							</button>
+							<button
+								onClick={confirmClose}
+								className="px-4 py-1 text-sm flex justify-center items-center bg-[#f24e1e]/80 border border-[#f24e1e]/60 text-white rounded-lg hover:bg-[#f24e1e]/90 transition-all duration-300"
+							>
+								Confirm
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
+
+			{!isModalOpen && (
+				<div className="fixed inset-0 backdrop-blur-md bg-opacity-50 flex justify-center items-center z-50 transition-opacity duration-500 opacity-0 pointer-events-none"></div>
+			)}
 		</div>
 	);
 }
